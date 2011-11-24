977e1d53c1c2d5912aa8682defd9e55bae30c601
Bronson Zgeb <bronson.zgeb@gmail.com>
2011-11-24 09:18:49 -0500

    Completed the robot control interface.

---
+++ test.php
@@ -0,0 +1,49 @@
+<?php
+    $script = $_POST["script"];
+    //var_dump($script);
+
+    header("Content-type: text/plain");
+
+    foreach ($script as $operation) {
+        $opcode = $operation["opcode"];
+        $arg    = $operation["arg"];
+
+        switch ($opcode) {
+            case "fd":
+                echo "Forward ho!\n";
+                wait($arg);
+                break;
+            case "bw":
+                echo "Backward ho!\n";
+                wait($arg);
+                break;
+            case "rt":
+                echo "Right Turn ho!\n";
+                break;
+            case "lt":
+                echo "Left Turn ho!\n";
+                break;
+            case "pu":
+                echo "Pen Up!\n";
+                break;
+            case "pd":
+                echo "Pen Down!\n";
+                break;
+            default:
+                echo "DERP!!\n";
+                break;
+        }
+
+        echo $operation["opcode"];
+        echo " ";
+        echo $operation["arg"];
+        echo "\n";
+    }
+
+    function wait($time) {
+        echo "Waiting for " . $time . "...";
+        usleep($time * 1000);
+        echo "Done!";
+        echo "\n";
+    }
+?>
