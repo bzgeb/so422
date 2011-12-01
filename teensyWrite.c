a4ad0c960a138786096e79e83b271e3fc84586c9
Bronson Zgeb <bronson.zgeb@gmail.com>
2011-11-30 21:01:26 -0500

    Added c functions to read and write from the teensy

---
+++ teensyWrite.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int main(int argc,char** argv)
+{
+    struct termios tio;
+    int tty_fd;
+    fd_set rdset;
+
+//    unsigned char c = 'C';
+
+//    printf("Please start with %s /dev/ttyS1 (for example)\n",argv[0]);
+
+    memset(&tio,0,sizeof(tio));
+    tio.c_iflag=0;
+    tio.c_oflag=0;
+    tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+    tio.c_lflag=0;
+    tio.c_cc[VMIN]=1;
+    tio.c_cc[VTIME]=5;
+
+    tty_fd=open(argv[1], O_RDWR | O_NONBLOCK);      
+    cfsetospeed(&tio,B9600);            //  baud
+    cfsetispeed(&tio,B9600);            //  baud
+
+    tcsetattr(tty_fd,TCSANOW,&tio);
+
+    write(tty_fd, argv[2], strlen(argv[2]));
+//
+//    while(1)
+//    {
+//        if(read(tty_fd, &c, 1)>0)// try to read a byte from the tty
+//        {
+//            printf("%c", c);
+//        }
+//    } 
+    close(tty_fd);
+}
